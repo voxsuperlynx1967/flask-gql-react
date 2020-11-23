c4664fbd180a28ab0fd9385e3b6b2ec27587ff93
Simon Carroll <snc40@georgetown.edu>
2020-11-23 15:17:26 -0500

    post endpoint

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
-from ariadne import graphql_sync, make_executable)schema, gql, load_schema_from_path
+from ariadne import graphql_sync, make_executable_schema, gql, load_schema_from_path
 from ariadne.constants import PLAYGROUND_HTML
-from model.py import query
-type_defs = gql(load_schema_from_path("./schema.graphql"))
+# from model import query
+# type_defs = gql(load_schema_from_path("./schema.graphql"))
 #load schema to create type definition, gql validates the schema and lets us know if anything is wrong
-schema = make_executabel_schema(type_defs, query)
+# schema = make_executabel_schema(type_defs, query)
 #we make it executable by combining the type definiton with query, our resolvers for all queries
 #playground is useful for development, writing testing and learning about our graphql API
 
@@ -14,9 +14,20 @@ app = Flask(__name__)
 # def hello():
 #      return 'Hello, World!'
 
-@app.route("/graphql". methods=["GET"])
+@app.route("/graphql", methods=["POST"])
 def graphql_playground():
-    return PLAYGROUND_HTML, 200
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 if __name__ =='__main__':
         app.run()
