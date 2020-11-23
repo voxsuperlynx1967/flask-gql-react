df1e9458a464927a07895a54d5d25ccd4e5db28c
Simon Carroll <snc40@georgetown.edu>
2020-11-23 13:51:46 -0500

    model and graphql playground

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, jsonify
 from ariadne import graphql_sync, make_executable)schema, gql, load_schema_from_path
-from  araidne.constants import PLAYGROUND_HTML
+from ariadne.constants import PLAYGROUND_HTML
+from model.py import query
+type_defs = gql(load_schema_from_path("./schema.graphql"))
+#load schema to create type definition, gql validates the schema and lets us know if anything is wrong
+schema = make_executabel_schema(type_defs, query)
+#we make it executable by combining the type definiton with query, our resolvers for all queries
 #playground is useful for development, writing testing and learning about our graphql API
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-     return 'Hello, World!'
+# @app.route("/")
+# def hello():
+#      return 'Hello, World!'
+
+@app.route("/graphql". methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
 
 if __name__ =='__main__':
         app.run()
