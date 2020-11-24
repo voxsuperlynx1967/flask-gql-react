8107999548ba0d93a8e7b5c67ee5ff16c8eea3c3
Simon Carroll <snc40@georgetown.edu>
2020-11-23 20:12:24 -0500

    style changes

--- client/src/Order.js
+++ client/src/Order.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
+import './order.css'
 export default function Order(order) {
     return (
-        <div id={order.id}>
-            <div>
+        <div className="order" id={order.id}>
+            <div className="name">
              {order.name}
             </div>
-            <div>
+            <div className="info">
             {order.size.toLowerCase()}{" "} of {order.type.split("_").join(" ").toLowerCase()}
             </div>
         </div>
